0d33ce6c7c384e9eb92dbf8f95c66ead6bed382f
Ruth <emailtoruth@virginmedia.com>
2015-10-21 15:40:14 +0100

    setting up api request

--- handlers.js
+++ handlers.js
@@ -6,8 +6,8 @@ var index = fs.readFileSync(__dirname + '/public/index.html');
 var env = require('env2')('./config.env');
 var apiId = process.env.apiId;
 var apiKey = process.env.apiKey;
-
-var tubeLine = ['victoria', 'bakerloo'];
+//
+// var line =
 
 var headers = {
     'content-type' : 'text/html'
@@ -25,20 +25,22 @@ handlers.file = function(req, res){
     res.end(file);
 };
 
-handlers.api = function(req, res){
-  res.writeHead(200, headers);
-  request('https://api.tfl.gov.uk/Line/' + getRandomTubeLine() + '/StopPoints?app_id=' + apiId + '&app_key=' + apiKey, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-    console.log(body);
-    res.end(body);
-    }
+handlers.api = function(req, res) {
+  var userInput = "";
+  req.on("data", function(data) {
+    userInput += data;
+  });
+  req.on("end", function() {
+    res.writeHead(200, headers);
+    request('https://api.tfl.gov.uk/Line/' + userInput + '/StopPoints?app_id=' + apiId + '&app_key=' + apiKey, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        //console.log(body);
+        res.end(body);
+      }
+    });
   });
 };
 
-function getRandomTubeLine () {
-  var i = Math.floor((Math.random() * 13) + 1);
-  return tubeLine[i];
-}
 
 handlers.notFound = function(req, res){
     res.writeHead(404, headers);
--- public/frontend.js
+++ public/frontend.js
@@ -1,11 +1,20 @@
 var request = new XMLHttpRequest ();
+var lineInput = document.getElementById("userLine").value;
 
-(function () {
+function getLineData (lineInput) {
   request.onreadystatechange = function() {
     if (request.readyState == 4 && request.status === 200) {
       console.log(request.responseText);
     }
   };
-  request.open("GET", '/apirequest');
-  request.send();
-}());
+  request.open("POST", '/apirequest');
+  request.send(lineInput);
+  console.log('GETDATA FUNCTION >>>>>>>',lineInput);
+}
+
+document.getElementById("userForm").addEventListener("submit", function (e){
+  e.preventDefault();
+  var lineInput = document.getElementById("userLine").value;
+  getLineData(lineInput);
+  console.log(lineInput);
+});
