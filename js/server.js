bc2d6fdb8052db4147223e2acb6fffce1acdbaf0
Ruth <emailtoruth@virginmedia.com>
2015-10-22 19:01:54 +0100

    changing port for heroku

--- js/server.js
+++ js/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var router  = require('./router.js');
-var port = process.env.port || 8000;
+var port = process.env.PORT || 8000;
 var app = function(req, res) {
     router(req, res);
 };
